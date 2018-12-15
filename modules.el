22d98e59190d2f68224222b72457ab089ecc392d
Sean Matthews <sean.d.matthews@gmail.com>
2018-12-15 00:35:56 -0500

    Initial commit

---
+++ modules.el
@@ -0,0 +1,17 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Modules list
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Set module names from the modules directory that you'd like to use.
+;; By default, all moduels will be loaded for your configuration.
+(setq whitelist
+	  '(*)
+	  )
+
+;; Set modules names from the modules directory that you wish to ignore.
+;; This list overides modules that have been listed in the whitelist.
+;; By default no modules will be excluded from your configuration.
+(setq blacklist
+	  '()
+	  )
+		

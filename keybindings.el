631db8da2b1bad08ee26ec99463069536a87f068
Sean Matthews <smatthews@exyntechnologies.com>
2019-12-05 17:09:30 -0500

    Takes config off of F12

--- keybindings.el
+++ keybindings.el
@@ -1,3 +1,6 @@
 ;; open .emacs file with F12
 ;; put this at top so it loads even if you have other problems in this file
-(global-set-key (kbd "<f12>") (lambda () (interactive) (find-file-other-window user-init-file)))
+(global-set-key (kbd "<M-f12>") (lambda () (interactive) (find-file-other-window user-init-file)))
+
+;;
+(global-set-key [remap kill-ring-save] 'easy-kill)
